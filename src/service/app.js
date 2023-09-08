48c4b76e9725704c4ad56dda91f6f8548e3382b4
GetcharZp <2272795814@qq.com>
2023-09-08 11:49:53 +0800

    feat 支持文件下载

--- src/service/app.js
+++ src/service/app.js
@@ -1,5 +1,5 @@
-const { ipcMain } = require('electron')
-const { v4: uuidv4 } = require('uuid')
+const {ipcMain, dialog} = require('electron')
+const {v4: uuidv4} = require('uuid')
 const Store = require('electron-store')
 
 // 初始化存储对象
@@ -43,6 +43,29 @@ const newAppService = () => {
         store.set(connectionKey, connections);
         return {"code": 200, "msg": "success"};
     })
+
+    // 打开保存文件的弹出框
+    ipcMain.handle('showSaveDialog', async (_,req) => {
+        const options = {
+            title: '选择文件保存位置',
+            defaultPath: req['filename'], // 默认文件名
+            buttonLabel: '保存', // 按钮显示的文本
+            filters: [
+                {name: '文本文件', extensions: ['txt']},
+                {name: '所有文件', extensions: ['*']}
+            ],
+            properties: ['showOverwriteConfirmation'] // 可选，添加此属性以显示覆盖确认对话框
+        };
+
+        try {
+            const result = await dialog.showSaveDialog(options);
+            if (!result.canceled) {
+                return result.filePath;
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    });
 
     return ipcMain
 }
